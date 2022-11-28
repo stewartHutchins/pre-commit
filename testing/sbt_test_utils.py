7bfe3ed0f4c1635eeec4a6e17c5164032405be62
Stewart Hutchins <stewartmhutchins@gmail.com>
2022-11-28 18:47:38 +0000

    Run test_sbt_hook with sbt server running and sbt not running.

---
+++ testing/sbt_test_utils.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import asyncio
+import subprocess
+from asyncio.subprocess import Process
+from pathlib import Path
+
+_TIMEOUT = 30
+
+
+async def start_sbt_server(
+        root_dir: Path,
+        shutdown_timeout: int = _TIMEOUT,
+) -> Process:
+    process = await asyncio.create_subprocess_shell(
+        'sbt',
+        cwd=root_dir,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        shell=True,
+    )
+    await asyncio.wait_for(
+        _wait_until_server_started(root_dir),
+        timeout=shutdown_timeout,
+    )
+    return process
+
+
+async def shutdown_sbt_server(
+        proc: Process,
+        *,
+        timeout: int = _TIMEOUT,
+) -> None:
+    try:
+        await asyncio.wait_for(
+            proc.communicate(b'shutdown\n'), timeout=timeout,
+        )
+    except asyncio.TimeoutError:
+        proc.kill()
+
+
+async def _wait_until_server_started(root_dir: Path) -> None:
+    while not _is_server_running(root_dir):
+        await asyncio.sleep(1)
+
+
+_ACTIVE_JSON_PATH = 'project/target/active.json'
+
+
+def _port_file_path(root_dir: Path) -> Path:
+    return root_dir.joinpath(_ACTIVE_JSON_PATH)
+
+
+def _is_server_running(root_dir: Path) -> bool:
+    return _port_file_path(root_dir).exists()
